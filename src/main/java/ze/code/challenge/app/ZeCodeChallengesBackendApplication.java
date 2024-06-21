734c5ea997247778259e707a608759962917d64c
Thiago Ribeiro <thiagoribeirods@outlook.com>
2024-06-21 20:29:11 -0300

    initial build commit

---
+++ src/main/java/ze/code/challenge/app/ZeCodeChallengesBackendApplication.java
@@ -0,0 +1,13 @@
+package ze.code.challenge.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeCodeChallengesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeCodeChallengesBackendApplication.class, args);
+    }
+
+}
