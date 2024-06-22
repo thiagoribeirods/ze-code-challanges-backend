1ccfabc928c9f768a9c2715ebd52d5819e5961c5
Thiago Ribeiro <thiagoribeirods@outlook.com>
2024-06-22 16:16:33 -0300

    fixed invalid/not implemented entities

--- src/main/java/ze/code/challenge/app/ZeCodeChallengesBackendApplication.java
+++ src/main/java/ze/code/challenge/app/ZeCodeChallengesBackendApplication.java
@@ -1,12 +1,7 @@
 package ze.code.challenge.app;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ze.code.challenge.app.entity.Address;
-import ze.code.challenge.app.entity.Area;
-import ze.code.challenge.app.entity.Partner;
 
 @SpringBootApplication
 public class ZeCodeChallengesBackendApplication {
