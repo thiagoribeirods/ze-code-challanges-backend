ae54b07fb7a7e452a610d2626bd4799554cae0ba
Thiago Ribeiro <thiagoribeirods@outlook.com>
2024-06-24 12:55:44 -0300

    Swagger documentation has been fully implemented for the API endpoints

--- src/main/java/ze/code/challenge/app/infrastructure/controller/PartnerController.java
+++ src/main/java/ze/code/challenge/app/infrastructure/controller/PartnerController.java
@@ -1,5 +1,9 @@
 package ze.code.challenge.app.infrastructure.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
@@ -21,7 +25,11 @@ public class PartnerController {
     private final GetPartnerById getPartnerById;
     private final SearchPartnerByCoordinates searchPartnerByCoordinates;
 
-    public PartnerController(CreatePartner createPartner, GetPartners getPartners, GetPartnerByDocument getPartnerByDocument, GetPartnerById getPartnerById, SearchPartnerByCoordinates searchPartnerByCoordinates) {
+    public PartnerController(CreatePartner createPartner,
+                             GetPartners getPartners,
+                             GetPartnerByDocument getPartnerByDocument,
+                             GetPartnerById getPartnerById,
+                             SearchPartnerByCoordinates searchPartnerByCoordinates) {
         this.createPartner = createPartner;
         this.getPartners = getPartners;
         this.getPartnerByDocument = getPartnerByDocument;
@@ -30,11 +38,18 @@ public class PartnerController {
     }
 
     @GetMapping
+    @Operation(summary = "Get partners", description = "Get list of partners")
+    @ApiResponse(description = "This method returns a list of Partners",
+                 responseCode = "200",
+                 content = @Content(mediaType = "application/json"))
     public ResponseEntity<?> get() {
         return ResponseEntity.ok(getPartners.execute());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get partner", description = "Get partner by ID if it exists")
+    @ApiResponse(description = "This method returns a Partner", responseCode = "200", content = @Content(mediaType = "application/json"))
+    @ApiResponse(description = "This method returns not found if Partner does not exists", responseCode = "404", content = @Content(mediaType = "application/json"))
     public ResponseEntity<?> get(@PathVariable String id) {
         Optional<Partner> optionalPartner = getPartnerById.execute(id);
 
@@ -46,11 +61,15 @@ public class PartnerController {
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Get a list of nearest partners", description = "Given a specific location (coordinates long and lat), search the nearest partner which the coverage area includes the location.")
     public ResponseEntity<?> getCoverageArea(@RequestParam double longitude, @RequestParam double latitude) {
         return ResponseEntity.ok(searchPartnerByCoordinates.execute(longitude, latitude));
     }
 
     @PostMapping
+    @Operation(summary = "Create a partner", description = "Create a new Partner")
+    @ApiResponse(description = "This method returns DuplicateKeyException if the Document already exists", responseCode = "400", content = @Content(mediaType = "application/json"))
+    @ApiResponse(description = "This method returns the created Partner", responseCode = "201", content = @Content(mediaType = "application/json"))
     public ResponseEntity<?> post(@RequestBody @Valid PartnerRequestDTO partnerRequestDTO) {
 
         if(getPartnerByDocument.execute(partnerRequestDTO.document()).isPresent()) {
