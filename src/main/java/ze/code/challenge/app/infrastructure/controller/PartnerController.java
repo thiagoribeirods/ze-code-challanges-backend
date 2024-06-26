2f09ae4c15a3a72b3348e6ddd99d101266205e74
Thiago Ribeiro <thiagoribeirods@outlook.com>
2024-06-25 22:27:35 -0300

    including schemas for the post request body example

--- src/main/java/ze/code/challenge/app/infrastructure/controller/PartnerController.java
+++ src/main/java/ze/code/challenge/app/infrastructure/controller/PartnerController.java
@@ -1,9 +1,11 @@
 package ze.code.challenge.app.infrastructure.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/partners")
+@Tag(name = "Partner Controller", description = "This controller hosts all endpoints for this API")
 public class PartnerController {
 
     private final CreatePartner createPartner;
@@ -60,7 +63,7 @@ public class PartnerController {
         return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/search")
+    @GetMapping("/nearest")
     @Operation(summary = "Get a list of nearest partners", description = "Given a specific location (coordinates long and lat), search the nearest partner which the coverage area includes the location.")
     public ResponseEntity<?> getCoverageArea(@RequestParam double longitude, @RequestParam double latitude) {
         return ResponseEntity.ok(searchPartnerByCoordinates.execute(longitude, latitude));
@@ -69,7 +72,10 @@ public class PartnerController {
     @PostMapping
     @Operation(summary = "Create a partner", description = "Create a new Partner")
     @ApiResponse(description = "This method returns DuplicateKeyException if the Document already exists", responseCode = "400", content = @Content(mediaType = "application/json"))
-    @ApiResponse(description = "This method returns the created Partner", responseCode = "201", content = @Content(mediaType = "application/json"))
+    @ApiResponse(responseCode = "201", description = "This method returns the created Partner", content = @Content(mediaType = "application/json"))
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Example of Partner",
+            content = @Content(schema = @Schema(implementation = Partner.class),
+            examples = @ExampleObject(value = "{\"tradingName\": \"Adega da Cerveja - Pinheiros\", \"ownerName\": \"Zé da Silva\", \"document\": \"1432132123891/0001\", \"coverageArea\": { \"type\": \"MultiPolygon\", \"coordinates\": [[[[30, 20], [45, 40], [10, 40], [30, 20]]], [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]] }, \"address\": { \"type\": \"Point\", \"coordinates\": [-46.57421, -21.785741] }}")))
     public ResponseEntity<?> post(@RequestBody @Valid PartnerRequestDTO partnerRequestDTO) {
 
         if(getPartnerByDocument.execute(partnerRequestDTO.document()).isPresent()) {
